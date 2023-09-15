9fe607099c86b13bcfdbccee78846c285e05e2b6
harshithadev <harshithadevineni16@gmail.com>
2023-09-15 19:06:39 +0530

    categories and quotes added from source file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ const CATEGORIES = [
   { category: "life", color: "#9b2226" },
 ];
 
+const initialQuotes = [
+  {
+    id: 1,
+    quoteText:
+      "Love is that condition in which the happiness of another person is essential to your own.",
+    origin:
+      "https://www.goodreads.com/work/quotes/908211-stranger-in-a-strange-land",
+    originName: "Stranger in a strange land",
+    category: "love",
+    votesThumbsUp: 27,
+    votesMindblowing: 9,
+    votesFalse: 4,
+  },
+];
+
 function App() {
   return (
     <Fragment>
@@ -42,45 +57,80 @@ function QuoteForm() {
 }
 
 function CategoryFilters() {
-  return <aside>Category Filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li>
+          <button
+            className="btn btn-large btn-category tag"
+            style={{ backgroundColor: "#e9d8a6" }}
+          >
+            all
+          </button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li>
+            <button
+              className="btn btn-large btn-category tag"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.category}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function QuotesList() {
+  const quotes = initialQuotes;
+
   return (
     <section>
       <ul className="quotes-list">
-        <li className="quote">
-          <p>
-            Love is that condition in which the happiness of another person is
-            essential to your own.
-            <a
-              className="source"
-              target="_blank"
-              href="https://www.goodreads.com/work/quotes/908211-stranger-in-a-strange-land"
-            >
-              Stranger in a Strange Land
-            </a>
-          </p>
-          <span className="tag" style={{ backgroundColor: "#bb3e03" }}>
-            {" "}
-            Love
-          </span>
-          <div className="votes">
-            <button className="btn">
-              👍 <span>5</span>
-            </button>
-
-            <button className="btn">
-              🤯 <span>3</span>
-            </button>
-
-            <button className="btn">
-              ❌ <span>1</span>
-            </button>
-          </div>
-        </li>
+        {quotes.map((quote) => (
+          <Quote key={quote.id} quote={quote} />
+        ))}
       </ul>
     </section>
+  );
+}
+
+function Quote({ quote }) {
+  return (
+    <li className="quote">
+      <p>
+        {quote.quoteText}
+        <a className="source" target="_blank" href={quote.origin}>
+          {quote.originName}
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find(
+            (cat) => cat.category == quote.category
+          ).color,
+        }}
+      >
+        {" "}
+        {quote.category}
+      </span>
+      <div className="votes">
+        <button className="btn">
+          👍 <span>{quote.votesThumbsUp}</span>
+        </button>
+
+        <button className="btn">
+          🤯 <span>{quote.votesMindblowing}</span>
+        </button>
+
+        <button className="btn">
+          ❌ <span>{quote.votesFalse}</span>
+        </button>
+      </div>
+    </li>
   );
 }
 
